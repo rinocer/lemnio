9a323a1ec3e4d1931d59010ff34d6b8a2dd8f5ec
Marian C Moldovan <marian.claudiu@beeva.com>
2017-06-14 01:13:01 +0200

    Landing 1.0

--- beta/js/custom.js
+++ beta/js/custom.js
@@ -22,6 +22,18 @@
         }
     });
 
+    $('.btn-link').bind('click', function(event) {
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+
+        event.preventDefault();
+        }
+    });
+
 
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
